414d1a3636003f0d5b68b2c24f8a25d8226fe06d
FlorinLacatusu <florin_lacatusu_94@yahoo.com>
2020-02-26 11:24:49 +0200

    Update server.js

--- nodevol/server.js
+++ nodevol/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/', (req, res) => res.send('Data from ConfigMap:'+ process.env.CM + ', '+'Data from Secret:'+ process.env.SCR ))
+app.get('/', (req, res) => res.json({DataFromConfigMap:process.env.CM, DatafromSecret: process.env.SCR}))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
